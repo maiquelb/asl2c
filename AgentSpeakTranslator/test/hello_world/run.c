f3bea8f080aa1ef8c62521a78340ff34f5e61eba
maiquel <maiquelb@gmail.com>
2019-05-23 08:29:52 -0300

    add run.c

---
+++ AgentSpeakTranslator/test/hello_world/run.c
@@ -0,0 +1,12 @@
+/*
+This program runs the agent hello_world.c.
+It is not produced by the translation.   
+*/
+
+#include "hello_world.c"
+
+int main(){
+   init();
+   while(1==1)
+      next_step();
+}
